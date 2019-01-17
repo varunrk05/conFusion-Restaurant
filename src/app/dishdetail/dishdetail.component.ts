a9d939da01d7e63bbb0326b91c50e33a8fb255d5
Varun <varunrajatkumar@gmail.com>
2019-01-18 01:25:06 +0530

    Single Page Applications 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
+
+import { DishService } from '../services/dish.service';
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
 import { Dish } from '../shared/dish';
 
 @Component({
@@ -8,12 +13,20 @@ import { Dish } from '../shared/dish';
 })
 export class DishdetailComponent implements OnInit {
 
-  @Input()
   dish: Dish;
 
-  constructor() { }
+  constructor(private dishService: DishService,
+    private route: ActivatedRoute,
+    private location: Location) { }
 
   ngOnInit() {
+    let id = +this.route.snapshot.params['id'];
+    //console.log(id);
+    this.dish = this.dishService.getDish(id);
+    //console.log(this.dish.name);
   }
 
+  goBack() : void {
+    this.location.back();
+  }
 }
--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -11,8 +11,8 @@ export class DishService {
     return DISHES;
   }
 
-  getDish(id: string) : Dish{
-    return DISHES.filter((dish) => {dish.id === id})[0];
+  getDish(id: number) : Dish{
+    return DISHES.filter((dish) => dish.id === id)[0];
   }
 
   getFeaturedDish() : Dish{
