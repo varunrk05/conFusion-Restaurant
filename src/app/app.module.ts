f78ea0d526b5d767de85a6ca96810a9b43bba205
Varun <varunrajatkumar@gmail.com>
2019-04-14 23:56:35 +0530

    Reactive Forms Part 3 - 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,11 +28,11 @@ import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
 
 import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
-import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
